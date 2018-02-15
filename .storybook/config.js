d12c233560c0ed40e0e7004b11c75f5d02884026
KunhoLee <gunho1020@gmail.com>
2018-02-15 17:58:21 +0900

    TST: storybook require 설정

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,15 @@
 import { configure } from '@storybook/react';
 
+const req = require.context(
+  '../src/components',
+  true,
+  /\.stories\.(js|jsx)$/,
+);
+
 function loadStories() {
-  require('../src/stories');
+  req.keys().forEach(filename => {
+    req(filename)
+  });
 }
 
 configure(loadStories, module);
