a596dd10dbd49b5dac2ba5ed707c1f2d8b7ba248
KunhoLee <gunho1020@gmail.com>
2018-02-15 20:57:00 +0900

    ENH: DisplayMarker 컴포넌트 playerMarker, computerMarker props 검사

--- src/components/molecules/DisplayMarker/presenter.js
+++ src/components/molecules/DisplayMarker/presenter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // import Components
 import P from '../../atoms/P';
@@ -7,5 +8,10 @@ const DisplayMarker = ({
   playerMarker,
   computerMarker,
 }) => <P>player: {playerMarker}, computer: {computerMarker}</P>;
+
+DisplayMarker.propTypes = {
+  playerMarker: PropTypes.string.isRequired,
+  computerMarker: PropTypes.string.isRequired,
+};
 
 export default DisplayMarker;
