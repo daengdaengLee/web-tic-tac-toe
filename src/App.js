9667194846d9bee70aa68572b9144d5a4ec7b9cb
KunhoLee <gunho1020@gmail.com>
2018-02-15 20:50:41 +0900

    TST: ChooseMarker, DisplayMarker 컴포넌트 reducer 와 연결 확인

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+// import reducers
+import gamesReducer from './ducks/games';
+
+// import Components
+import ChooseMarker from './components/molecules/ChooseMarker';
+import DisplayMarker from './components/molecules/DisplayMarker';
+
+// create store
+let store = createStore(gamesReducer);
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <Provider store={store}>
+      <div>
+        <DisplayMarker />
+        <ChooseMarker />
+      </div>
+    </Provider>
   );
 }
 
