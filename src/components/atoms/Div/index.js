3f76f02f66b27bfa904f923a022dd11f43f48c78
KunhoLee <gunho1020@gmail.com>
2018-02-16 11:46:59 +0900

    ENH: Div 컴포넌트 withValidStyleNames HOC 사용
    withValidProp -> withValidStyleNames
    구체적인 과정 숨김

--- src/components/atoms/Div/index.js
+++ src/components/atoms/Div/index.js
@@ -2,24 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // import HOCs
-import withValidProps from '../../../hoc/withValidProps';
+import withValidStyleNames from '../../../hoc/withValidStyleNames';
 
 // import Utils
 import makeClassName from '../../../utils/makeClassName';
-import validateStyleNames from '../../../utils/validateStyleNames';
 
 // import CSS
 import './index.css';
-
-// define validator for styleNames prop
-const validator = validateStyleNames(['MY__GameBoard']);
 
 const Temp = ({
   children,
   styleNames,
 }) => <div className={makeClassName(styleNames)}>{children}</div>;
 
-const Div = withValidProps(validator)(Temp);
+const Div = withValidStyleNames(['MY__GameBoard'])(Temp);
 
 Div.propTypes = {
   children: PropTypes.node,
