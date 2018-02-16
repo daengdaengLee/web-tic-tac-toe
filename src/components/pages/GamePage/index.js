4155bad35758ac4efea96deee750b26d22eaa3ef
KunhoLee <gunho1020@gmail.com>
2018-02-16 11:09:53 +0900

    ENH: GamePage 컴포넌트 작성

---
+++ src/components/pages/GamePage/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+// import presenter Component
+import GamePagePresenter from './presenter';
+
+function mapStateToProps(state) {
+  return { gameProcess: state.gameProcess };
+}
+
+export default connect(mapStateToProps)(GamePagePresenter);
