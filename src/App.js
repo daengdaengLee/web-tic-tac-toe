8a040ee072efcfe0d6b5d23c914bee063d233eaf
KunhoLee <gunho1020@gmail.com>
2018-02-16 22:30:03 +0900

    TST: mySaga 테스트

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware  from 'redux-saga';
 import { Provider } from 'react-redux';
 
 // import reducers
 import gamesReducer from './ducks/games';
 
+// import sagas
+import mySaga from './ducks/games/sagas';
+
 // import Components
-import DisplayMarker from './components/molecules/DisplayMarker';
-import ChooseMarker from './components/organisms/ChooseMarker';
-import GameBoard from './components/organisms/GameBoard';
+import GamePage from './components/pages/GamePage';
+
+// create saga middleware
+const sagaMiddleware = createSagaMiddleware();
 
 // create store
-let store = createStore(gamesReducer);
+const store = createStore(
+  gamesReducer,
+  applyMiddleware(sagaMiddleware)
+);
+
+// then run the saga
+sagaMiddleware.run(mySaga);
 
 function App() {
   return (
     <Provider store={store}>
-      <div>
-        <DisplayMarker />
-        <ChooseMarker />
-        <GameBoard />
-      </div>
+      <GamePage />
     </Provider>
   );
 }
