bfdeefc62bc6f9f9c638644197dfa72c1f25d7aa
KunhoLee <gunho1020@gmail.com>
2018-02-16 17:51:47 +0900

    ENH: GameBoard 컴포넌트에 selectSquare prop 추가
    Squae 컴포넌트에 id, selectSquare props 전달

--- src/components/organisms/GameBoard/presenter.js
+++ src/components/organisms/GameBoard/presenter.js
@@ -6,17 +6,23 @@ import Div from '../../atoms/Div';
 import Square from '../../molecules/Square';
 import DisplayMarker from '../../molecules/DisplayMarker';
 
-const GameBoard = ({ gameStatus }) => (
+const GameBoard = ({ gameStatus, selectSquare }) => (
   <Div>
     <DisplayMarker />
     <Div styleNames={['MY__GameBoard']}>
-      {gameStatus.map((value, index) => <Square mark={value} key={index} />)}
+      {gameStatus.map((value, index) => <Square
+        selectSquare={selectSquare}
+        id={index}
+        mark={value}
+        key={index}
+      />)}
     </Div>
   </Div>
 );
 
 GameBoard.propTypes = {
   gameStatus: PropTypes.arrayOf(PropTypes.oneOf(['X', 'O', ''])).isRequired,
+  selectSquare: PropTypes.func.isRequired,
 };
 
 export default GameBoard;
