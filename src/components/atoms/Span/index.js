9bd24075e8da4a959049b834808362a191cb67ba
KunhoLee <gunho1020@gmail.com>
2018-02-16 12:01:40 +0900

    ENH: Span 컴포넌트에 styleNames prop 추가
    - My__Square 스타일 추가

--- src/components/atoms/Span/index.js
+++ src/components/atoms/Span/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Span = ({ children }) => <span>{children}</span>;
+// import HOCs
+import withValidStyleNames from '../../../hoc/withValidStyleNames';
+
+// import Utils
+import makeClassName from '../../../utils/makeClassName';
+
+// import CSS
+import './index.css';
+
+const Temp = ({ children, styleNames }) => <span className={makeClassName(styleNames)}>{children}</span>;
+
+const Span = withValidStyleNames(['My__Square'])(Temp);
 
 Span.propTypes = {
   children: PropTypes.node,
+  styleNames: PropTypes.arrayOf(PropTypes.oneOf(['My__Square'])),
 };
 
 export default Span;
