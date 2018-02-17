981ecad546e65888df9bb24a5fd11718b55398d1
KunhoLee <gunho1020@gmail.com>
2018-02-17 12:25:55 +0900

    ENH: DisplayWinner 컴포넌트에 draw 일 경우 작성

--- src/components/molecules/DisplayWinner/presenter.js
+++ src/components/molecules/DisplayWinner/presenter.js
@@ -4,7 +4,10 @@ import PropTypes from 'prop-types';
 // import Components
 import P from '../../atoms/P';
 
-const DisplayWinnerPresenter = ({ winner }) => <P>Winner is {winner}!</P>;
+const DisplayWinnerPresenter = ({ winner }) => {
+  if(winner === 'draw') return <P>Draw!</P>;
+  return <P>Winner is {winner}!</P>;
+};
 
 DisplayWinnerPresenter.propTypes = {
   winner: PropTypes.string.isRequired,
