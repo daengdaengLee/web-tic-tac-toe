31628bc81840aa26ce77f9a2571828a321104b47
KunhoLee <gunho1020@gmail.com>
2018-02-17 13:26:53 +0900

    ENH: MainHeader 컴포넌트 수평 가운데 정렬

--- src/components/organisms/MainHeader/index.js
+++ src/components/organisms/MainHeader/index.js
@@ -5,7 +5,7 @@ import MainTitle from '../../molecules/MainTitle';
 import Header from '../../atoms/Header';
 
 const MainHeader = () => (
-  <Header>
+  <Header styleNames={['Horizontal__Center']}>
     <MainTitle />
   </Header>
 );
