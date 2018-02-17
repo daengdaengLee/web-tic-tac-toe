ea578608196ba5e5ce7abff0f742f6248844ed25
KunhoLee <gunho1020@gmail.com>
2018-02-17 13:26:14 +0900

    ENH: Header컴포넌트에 Horizontal__Center 스타일 추가

--- src/components/atoms/Header/index.js
+++ src/components/atoms/Header/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Header = ({ children }) => <header>{children}</header>;
+// import HOCs
+import withValidStyleNames from '../../../hoc/withValidStyleNames';
+
+// import Utils
+import makeClassName from '../../../utils/makeClassName';
+
+// import CSS
+import './index.css';
+
+const Temp = ({ children, styleNames }) => (
+  <header
+    className={makeClassName(styleNames)}
+  >
+    {children}
+  </header>
+);
+
+const validStyleNames = ['Horizontal__Center'];
+
+const Header = withValidStyleNames(validStyleNames)(Temp);
 
 Header.propTypes = {
   children: PropTypes.node.isRequired,
+  styleNames: PropTypes.arrayOf(PropTypes.oneOf(validStyleNames)),
 };
 
 export default Header;
